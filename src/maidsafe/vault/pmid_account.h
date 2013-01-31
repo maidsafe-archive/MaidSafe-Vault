cd31357393ea5c24eb19c2018a67e32360cc8d65
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2013-01-31 14:24:24 +0000

    Updated PmidAccount in line with planned NodeUp/NodeDown work.

--- src/maidsafe/vault/pmid_account.h
+++ src/maidsafe/vault/pmid_account.h
@@ -12,7 +12,9 @@
 #ifndef MAIDSAFE_VAULT_PMID_ACCOUNT_H_
 #define MAIDSAFE_VAULT_PMID_ACCOUNT_H_
 
-#include <map>
+#include <cstdint>
+#include <deque>
+#include <vector>
 
 #include "boost/filesystem/path.hpp"
 
@@ -29,30 +31,62 @@ namespace vault {
 
 class PmidAccount {
  public:
+  struct DataElement {
+    DataElement();
+    DataElement(const DataNameVariant& data_name_variant_in, int32_t size_in);
+    DataElement(const DataElement& other);
+    DataElement& operator=(const DataElement& other);
+    DataElement(DataElement&& other);
+    DataElement& operator=(DataElement&& other);
+
+    DataNameVariant data_name_variant;
+    int32_t size;
+  };
+  enum class DataHolderStatus : int32_t { kDown, kGoingDown, kUp, kGoingUp };
   typedef PmidName name_type;
-  typedef TaggedValue<NonEmptyString, struct SerialisedMaidAccountTag> serialised_type;
+  typedef TaggedValue<NonEmptyString, struct SerialisedPmidAccountTag> serialised_type;
   PmidAccount(const PmidName& pmid_name, const boost::filesystem::path& root);
   PmidAccount(const serialised_type& serialised_pmid_account, const boost::filesystem::path& root);
+
+  // Returns true if notification to MetadataManagers should begin (i.e. state changed from kDown)
+  bool DataHolderUp();
+  // Returns true if notification to MetadataManagers should begin (i.e. state changed from kUp)
+  bool DataHolderDown();
+  int32_t GetArchiveFileCount() const;
+  std::vector<DataElement> ParseArchiveFile(int32_t index) const;
+
+  // Used when this vault is no longer responsible for the account
+  void ArchiveAccount();
+  void RestoreAccount();
+
+  // Used in synchronisation with other Pmid Account Holders - serialises all in-memory data
+  serialised_type Serialise() const;
   std::vector<boost::filesystem::path> GetArchiveFileNames() const;
   NonEmptyString GetArchiveFile(const boost::filesystem::path& path) const;
   void PutArchiveFile(const boost::filesystem::path& path, const NonEmptyString& content);
 
+  // Throw if the data is a duplicate
   template<typename Data>
-  void PutData(const typename Data::name_type& name, int32_t size, int32_t replication_count);
+  std::future<void> PutData(const typename Data::name_type& name, int32_t size);
   template<typename Data>
-  bool DeleteData(const typename Data::name_type& name);
-  template<typename Data>
+  std::future<void> DeleteData(const typename Data::name_type& name);
 
-//  MaidName name() const { return pmid_record_; }
-  int64_t total_data_stored_by_pmids() const { return pmid_record_.stored_total_size; }
-//  int64_t total_put_data() const { return total_put_data_; }
+  name_type name() const { return pmid_record_.pmid_name; }
+  DataHolderStatus data_holder_status() const { return data_holder_status_; }
+
  private:
   PmidAccount(const PmidAccount&);
   PmidAccount& operator=(const PmidAccount&);
   PmidAccount(PmidAccount&&);
   PmidAccount& operator=(PmidAccount&&);
+
+  // Used when Data Holder has gone down, but this vault is still responsible for the account.
+  void ArchiveRecentData();
+  void RestoreRecentData();
+
   PmidRecord pmid_record_;
-  std::map<DataNameVariant, int32_t> recent_data_stored_;
+  DataHolderStatus data_holder_status_;
+  std::deque<DataElement> recent_data_stored_;
   DiskBasedStorage archive_;
 };
 
