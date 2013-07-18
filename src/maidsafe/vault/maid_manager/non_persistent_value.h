c6d8cb9c4718314afaf313ca3d2466f114645e8c
mmoadeli <mmoadeli@maidsafe.net>
2013-07-18 23:50:04 +0100

    implementing Maidmanager non persistent value (wip)

---
+++ src/maidsafe/vault/maid_manager/non_persistent_value.h
@@ -0,0 +1,59 @@
+/* Copyright 2012 MaidSafe.net limited
+
+This MaidSafe Software is licensed under the MaidSafe.net Commercial License, version 1.0 or later,
+and The General Public License (GPL), version 3. By contributing code to this project You agree to
+the terms laid out in the MaidSafe Contributor Agreement, version 1.0, found in the root directory
+of this project at LICENSE, COPYING and CONTRIBUTOR respectively and also available at:
+
+http://www.novinet.com/license
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is
+distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied. See the License for the specific language governing permissions and limitations under the
+License.
+*/
+
+#ifndef MAIDSAFE_VAULT_MAID_MANAGER_NON_PERSISTENT_VALUE_H_
+#define MAIDSAFE_VAULT_MAID_MANAGER_NON_PERSISTENT_VALUE_H__
+
+#include <cstdint>
+#include <set>
+#include <vector>
+
+#include "maidsafe/vault/maid_manager/helpers.h"
+#include "maidsafe/common/types.h"
+#include "maidsafe/vault/data_manager/data_manager.pb.h"
+#include "maidsafe/vault/types.h"
+
+namespace maidsafe {
+
+namespace vault {
+
+class MaidManagerNonPersistentValue {
+  typedef TaggedValue<NonEmptyString, struct MaidManagerNonPersistentValueTag> serialised_type;
+  explicit MaidManagerNonPersistentValue(const serialised_type& serialised_metadata_value);
+  MaidManagerNonPersistentValue(const int64_t& total_put_data,
+                                const std::vector<PmidTotals> pmid_totals);
+  serialised_type Serialise() const;
+
+  void RegisterPmid(const nfs::PmidRegistration& pmid_registration);  // kRegisterPmid
+  void UnregisterPmid(const PmidName& pmid_name);  // kUnregisterPmid
+  void UpdatePmidTotals(const PmidRecord& pmid_record); // Update Pmid Totals
+
+// friend bool operator==(const MaidManagerNonPersistentValue& lhs,
+//                        const MaidManagerNonPersistentValue& rhs);
+
+ private:
+  std::vector<PmidTotals>::iterator Find(const PmidName& pmid_name);
+  int64_t total_put_data_;
+  std::vector<PmidTotals> pmid_totals_;
+};
+
+//bool operator==(const MaidManagerNonPersistentValue& lhs, const MaidManagerNonPersistentValue& rhs);
+
+}  // namespace vault
+
+}  // namespace maidsafe
+
+
+#endif  // MAIDSAFE_VAULT_MAID_MANAGER_NON_PERSISTENT_VALUE_H_
