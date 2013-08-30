525daca9a69de520692ec0cccdf3a6f4c0cddedc
moadeli <mmoadeli@maidsafe.net>
2013-08-30 11:35:55 +0100

    Further tidying up

--- src/maidsafe/vault/accumulator-inl.h
+++ src/maidsafe/vault/accumulator-inl.h
@@ -43,7 +43,7 @@ template<typename T>
 typename Accumulator<T>::AddResult Accumulator<T>::AddPendingRequest(
     const T& request,
     const routing::GroupSource& source,
-    AddCheckerFunctor predicate) {
+    AddCheckerFunctor checker) {
   if (CheckHandled(request))
     return Accumulator<T>::AddResult::kHandled;
   bool already_exists(false);
@@ -62,7 +62,7 @@ typename Accumulator<T>::AddResult Accumulator<T>::AddPendingRequest(
     if (pending_requests_.size() > kMaxPendingRequestsCount_)
       handled_requests_.pop_front();
   }
-  return predicate(Get(request));
+  return checker(Get(request));
 }
 
 template<typename T>
@@ -116,159 +116,6 @@ std::vector<T> Accumulator<T>::Get(const T& request) {
   }
   return std::move(requests);
 }
-
-
-//template<typename Name>
-//typename std::deque<typename Accumulator<Name>::HandledRequest>::const_iterator
-//    Accumulator<Name>::FindHandled(const nfs::Message& message) const {
-//  return std::find_if(std::begin(handled_requests_),
-//                      std::end(handled_requests_),
-//                      [&message](const HandledRequest& handled_request) {
-//                      return (handled_request.msg_id == message.message_id()) &&
-//                             (handled_request.account_name ==
-//                                 Name(Identity(message.source().node_id.string())));
-//                      });
-//}
-
-//template<typename Name>
-//bool Accumulator<Name>::CheckHandled(const nfs::Message& message, nfs::Reply& reply_out) const {
-//  const auto it(FindHandled(message));  // NOLINT (dirvine)
-//  if (it != std::end(handled_requests_)) {
-//    if (it->reply.error().code() == CommonErrors::success)
-//      reply_out = it->reply;
-//    else
-//      reply_out = nfs::Reply(it->reply.error(), message.Serialise().data);
-//    return true;
-//  }
-//  return false;
-//}
-
-//template<typename Name>
-//std::vector<nfs::Reply> Accumulator<Name>::PushSingleResult(
-//    const nfs::Message& message,
-//    const routing::ReplyFunctor& reply_functor,
-//    const nfs::Reply& reply) {
-//  std::vector<nfs::Reply> replies;
-//  if (FindHandled(message) != std::end(handled_requests_))
-//    return replies;
-
-//  PendingRequest pending_request(message, reply_functor, reply);
-//  pending_requests_.push_back(pending_request);
-//  for (auto& request : pending_requests_) {
-//    if (request.msg.message_id() == message.message_id() &&
-//        request.msg.source().node_id == message.source().node_id) {
-//      replies.emplace_back(request.reply);
-//    }
-//  }
-//  // TODO(Profiling) - Consider holding requests in a map/set and each having a timestamp.
-//  // Pruning could then be done periodically (event-based) and would allow sorted list of requests.
-//  if (pending_requests_.size() > kMaxPendingRequestsCount_)
-//    pending_requests_.pop_front();
-//  return replies;
-//}
-
-//template<typename Name>
-//std::vector<nfs::Reply> Accumulator<Name>::GetPendingResults(const nfs::Message& message) const {
-//  std::vector<nfs::Reply> replies;
-//  if (FindHandled(message) != std::end(handled_requests_))
-//    return replies;
-
-//  for (auto& request : pending_requests_) {
-//    if (request.msg.message_id() == message.message_id() &&
-//        request.msg.source().node_id == message.source().node_id) {
-//      replies.emplace_back(request.reply);
-//    }
-//  }
-//  return replies;
-//}
-
-//template<typename Name>
-//std::vector<typename Accumulator<Name>::PendingRequest> Accumulator<Name>::SetHandled(
-//    const nfs::Message& message,
-//    const nfs::Reply& reply) {
-//  std::vector<PendingRequest> ret_requests;
-//  auto itr = pending_requests_.begin();
-//  while (itr != pending_requests_.end()) {
-//    if ((*itr).msg.message_id() == message.message_id() &&
-//        (*itr).msg.source().node_id == message.source().node_id) {
-//      ret_requests.push_back(*itr);
-//      itr = pending_requests_.erase(itr);
-//    } else {
-//      ++itr;
-//    }
-//  }
-
-//  handled_requests_.push_back(
-//      Accumulator::HandledRequest(message.message_id(),
-//                                  Name(Identity(message.source().node_id.string())),
-//                                  message.data().action,
-//                                  message.data().name,
-//                                  *message.data().type,
-//                                  static_cast<int32_t>(message.data().content.string().size()),
-//                                  reply));
-//  if (handled_requests_.size() > kMaxHandledRequestsCount_)
-//    handled_requests_.pop_front();
-//  return ret_requests;
-//}
-
-//template<typename Name>
-//void Accumulator<Name>::SetHandledAndReply(const nfs::MessageId message_id,
-//                                           const NodeId& source_node,
-//                                           const nfs::Reply& reply_out) {
-//  auto return_code(CommonErrors::success);
-//  nfs::Reply reply(return_code);
-//  std::vector<PendingRequest> ret_requests;
-//  auto itr = pending_requests_.begin();
-//  while (itr != pending_requests_.end()) {
-//    if ((*itr).msg.message_id() == message_id &&
-//        (*itr).msg.source().node_id == source_node) {
-//      ret_requests.push_back(*itr);
-//      itr = pending_requests_.erase(itr);
-//    } else {
-//      ++itr;
-//    }
-//  }
-
-//  handled_requests_.push_back(
-//      Accumulator::HandledRequest(message_id,
-//                                  Name(Identity(source_node.string())),
-//                                  reply_out));
-//  if (handled_requests_.size() > kMaxHandledRequestsCount_)
-//    handled_requests_.pop_front();
-//  // send replies if required
-//  for (const auto& ret : ret_requests) {
-//    if (ret.reply_functor)
-//      ret.reply_functor(reply_out.Serialise()->string());
-//  }
-//}
-
-
-
-// Workaround for gcc 4.6 bug related to warning "redundant redeclaration" for template
-// specialisation. refer // http://gcc.gnu.org/bugzilla/show_bug.cgi?id=15867#c4
-
-//#ifdef __GNUC__
-//#  pragma GCC diagnostic push
-//#  pragma GCC diagnostic ignored "-Wredundant-decls"
-//#endif
-//template<>
-//typename std::deque<typename Accumulator<DataNameVariant>::HandledRequest>::const_iterator
-//    Accumulator<DataNameVariant>::FindHandled(const nfs::Message& message) const;
-
-//template<>
-//std::vector<typename Accumulator<DataNameVariant>::PendingRequest>
-//    Accumulator<DataNameVariant>::SetHandled(
-//        const nfs::Message& message,
-//        const nfs::Reply& reply);
-
-//template<>
-//std::vector<typename Accumulator<PmidName>::PendingRequest> Accumulator<PmidName>::SetHandled(
-//    const nfs::Message& message,
-//    const nfs::Reply& reply);
-//#ifdef __GNUC__
-//#  pragma GCC diagnostic pop
-//#endif
-
 
 }  // namespace vault
 
--- src/maidsafe/vault/accumulator.h
+++ src/maidsafe/vault/accumulator.h
@@ -40,89 +40,6 @@ class AccumulatorTest_BEH_SetHandled_Test;
 class AccumulatorTest_BEH_FindHandled_Test;
 }  // namespace test
 
-
-//template<typename Name>
-//class Accumulator {
-// public:
-//  struct PendingRequest {
-//    PendingRequest(const nfs::Message& msg_in,
-//                   const routing::ReplyFunctor& reply_functor_in,
-//                   const nfs::Reply& reply_in);
-//    PendingRequest(const PendingRequest& other);
-//    PendingRequest& operator=(const PendingRequest& other);
-//    PendingRequest(PendingRequest&& other);
-//    PendingRequest& operator=(PendingRequest&& other);
-
-//    nfs::Message msg;
-//    routing::ReplyFunctor reply_functor;
-//    nfs::Reply reply;
-//  };
-
-//  struct HandledRequest {
-//    HandledRequest(const nfs::MessageId& msg_id_in,
-//                   const Name& account_name_in,
-//                   const nfs::MessageAction& action_type_in,
-//                   const Identity& data_name_in,
-//                   const DataTagValue& data_type_in,
-//                   const int32_t& size_in,
-//                   const nfs::Reply& reply_in);
-//    HandledRequest(const nfs::MessageId& msg_id_in,
-//                   const Name& account_name_in,
-//                   const nfs::Reply& reply_in);
-//    HandledRequest(const HandledRequest& other);
-//    HandledRequest& operator=(const HandledRequest& other);
-//    HandledRequest(HandledRequest&& other);
-//    HandledRequest& operator=(HandledRequest&& other);
-
-//    nfs::MessageId msg_id;
-//    Name account_name;
-//    nfs::MessageAction action;
-//    Identity data_name;
-//    DataTagValue data_type;
-//    int32_t size;
-//    nfs::Reply reply;
-//  };
-
-//  typedef TaggedValue<NonEmptyString, struct SerialisedRequestsTag> serialised_requests;
-
-//  Accumulator();
-
-//  // Returns true and populates <reply_out> if the message has already been set as handled.  If the
-//  // corresponding return_code != success, the message gets set in the reply.
-//  bool CheckHandled(const nfs::Message& message, nfs::Reply& reply_out) const;
-//  std::vector<nfs::Reply> GetPendingResults(const nfs::Message& message) const;
-//  // Adds a request with its individual result, pending the overall result of the operation.
-//  std::vector<nfs::Reply> PushSingleResult(const nfs::Message& message,
-//                                           const routing::ReplyFunctor& reply_functor,
-//                                           const nfs::Reply& reply);
-//  // Marks the message as handled and returns all pending requests held with the same ID
-//  std::vector<PendingRequest> SetHandled(const nfs::Message& message,
-//                                         const nfs::Reply& reply);
-//  // This will set handled and reply with success if a reply functor exists
-//  // safe to call many times
-//  void SetHandledAndReply(const nfs::MessageId message_id,
-//                          const NodeId& source_node,
-//                          const nfs::Reply& reply_out);
-//  friend class test::AccumulatorTest_BEH_PushSingleResult_Test;
-//  friend class test::AccumulatorTest_BEH_PushSingleResultThreaded_Test;
-//  friend class test::AccumulatorTest_BEH_CheckPendingRequestsLimit_Test;
-//  friend class test::AccumulatorTest_BEH_CheckHandled_Test;
-//  friend class test::AccumulatorTest_BEH_SetHandled_Test;
-//  friend class test::AccumulatorTest_BEH_FindHandled_Test;
-
-// private:
-//  Accumulator(const Accumulator&);
-//  Accumulator& operator=(const Accumulator&);
-//  Accumulator(Accumulator&&);
-//  Accumulator& operator=(Accumulator&&);
-//  typename std::deque<HandledRequest>::const_iterator FindHandled(
-//      const nfs::Message& message) const;
-
-//  std::deque<PendingRequest> pending_requests_;
-//  std::deque<HandledRequest> handled_requests_;
-//  const size_t kMaxPendingRequestsCount_, kMaxHandledRequestsCount_;
-//};
-
 namespace {
 
 template <typename RequestType>
@@ -191,7 +108,7 @@ class Accumulator {
 
   AddResult AddPendingRequest(const T& request,
                               const routing::GroupSource& source,
-                              AddCheckerFunctor predicate);
+                              AddCheckerFunctor checker);
   bool CheckHandled(const T& request);
   void SetHandled(const T& request, const routing::GroupSource& source);
   std::vector<T> Get(const T& request);
