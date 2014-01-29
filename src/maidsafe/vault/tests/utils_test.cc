a807d93e63d10a865f124d00d1bced671969b02c
prakash <prakash@maidsafe.net>
2014-01-29 02:12:33 +0000

    Added PublicPmidGetter class to poll on multiple futures. (wip)

--- src/maidsafe/vault/tests/utils_test.cc
+++ src/maidsafe/vault/tests/utils_test.cc
@@ -21,6 +21,7 @@
 
 #include "boost/filesystem/operations.hpp"
 #include "boost/filesystem/path.hpp"
+#include <boost/thread/future.hpp>
 
 #include "maidsafe/common/test.h"
 #include "maidsafe/common/utils.h"
@@ -28,6 +29,8 @@
 
 #include "maidsafe/vault/utils.h"
 #include "maidsafe/vault/vault.h"
+
+#include "maidsafe/vault/tests/tests_utils.h"
 
 namespace maidsafe {
 
@@ -63,6 +66,130 @@ TEST(UtilsTest, BEH_FixedWidthStringSize2) { CheckToAndFromFixedWidthString<2>()
 TEST(UtilsTest, BEH_FixedWidthStringSize3) { CheckToAndFromFixedWidthString<3>(); }
 
 TEST(UtilsTest, BEH_FixedWidthStringSize4) { CheckToAndFromFixedWidthString<4>(); }
+
+// temp testing
+
+class PublicPmidGetter {
+ public :
+   PublicPmidGetter();
+   void AddEntry(passport::PublicPmid::Name pmid_name,
+                 boost::future<passport::PublicPmid>&& future,
+                 routing::GivePublicKeyFunctor give_key_functors);
+ private:
+  typedef std::pair<passport::PublicPmid::Name, routing::GivePublicKeyFunctor> PublicPmidRequest;
+  void Poll();
+  void Run();
+  void CallFunctor(const passport::PublicPmid& public_pmid);
+  std::mutex mutex_;
+  bool running_;
+  std::vector<PublicPmidRequest> requests_;
+  std::vector<boost::future<passport::PublicPmid>> futures_, pending_futures_;
+  //std::thread thread_;
+  std::future<void> worker_future_;
+};
+
+
+// =========================== TEMP test ===================================================
+
+PublicPmidGetter::PublicPmidGetter()
+    : mutex_(),
+      running_(false),
+      requests_(),
+      futures_(),
+      pending_futures_(),
+      worker_future_() {}
+
+void PublicPmidGetter::AddEntry(passport::PublicPmid::Name pmid_name,
+                                boost::future<passport::PublicPmid>&& future,
+                                routing::GivePublicKeyFunctor give_key) {
+
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    requests_.push_back(PublicPmidRequest(pmid_name, give_key));
+    pending_futures_.push_back(std::move(future));
+    Run();
+  }
+
+}
+
+void PublicPmidGetter::Run() {
+  if (!running_) {
+      std::cout << std::endl << "starting thread !" << std::endl;
+    running_ = true;
+    worker_future_ = std::async(std::launch::async, [this]() { this->Poll(); });
+  }
+}
+
+void PublicPmidGetter::Poll() {
+  do {
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      std::move(pending_futures_.begin(), pending_futures_.end(),
+                std::back_inserter(futures_));
+      pending_futures_.resize(0);
+      if (futures_.size() == 0U) {
+        running_ = false;
+        break;
+      }
+    }
+    std::cout << std::endl << " polling on (" << futures_.size() << ") futures" << std::endl;
+    auto ready_future_itr = boost::wait_for_any(futures_.begin(), futures_.end());
+    //passport::PublicPmid public_pmid;
+    //try {
+    auto public_pmid = ready_future_itr->get();
+    //} catch (...) {
+     // FIXME Need to know the pmid name here to call right functor
+    //}
+    futures_.erase(ready_future_itr);
+    CallFunctor(public_pmid);
+  } while (true);
+}
+
+void PublicPmidGetter::CallFunctor(const passport::PublicPmid &public_pmid) {
+  std::lock_guard<std::mutex> lock(mutex_);
+  auto itr = std::find_if(requests_.begin(), requests_.end(),
+                          [public_pmid](const PublicPmidRequest& request){
+    return request.first == public_pmid.name();
+  });
+  if (itr != requests_.end())
+    itr->second(public_pmid.public_key());
+  requests_.erase(itr);
+}
+
+void RunFutureTestInParallel(int thread_count, std::function<void()> functor) {
+  std::vector<std::thread> threads;
+  for (int i = 0; i < thread_count; ++i)
+    threads.push_back(std::move(std::thread([functor]() { functor(); } )));
+  for (auto& thread : threads)
+    thread.join();
+}
+
+TEST(UtilsTest, BEH_FutureWaitForAny) {
+  PublicPmidGetter public_pmid_getter;
+  auto pmid(MakePmid());
+  passport::PublicPmid::Name pmid_name(PmidName(pmid.name()));
+  auto test = [pmid, pmid_name, &public_pmid_getter]() {
+    std::vector<boost::promise<passport::PublicPmid>> promises;
+    for (int i(0); i < 10 ; ++i) {
+      boost::promise<passport::PublicPmid> promise;
+      auto future = promise.get_future();
+      routing::GivePublicKeyFunctor functor = [i](asymm::PublicKey /*public_key*/) {
+        std::cout << std::endl << "called functor" << i << std::endl;
+      };
+      public_pmid_getter.AddEntry(pmid_name, std::move(future), functor);
+      promises.push_back(std::move(promise));
+      if (i == 5)
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+    for (auto& i : promises)
+      i.set_value(passport::PublicPmid(pmid));
+    };
+  RunFutureTestInParallel(10, test);
+  std::this_thread::sleep_for(std::chrono::seconds(2));
+  RunFutureTestInParallel(2, test);
+
+  // TODO Extend test and fix exception handling mechanism
+}
 
 }  // namespace test
 
