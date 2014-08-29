d276826400f1bf5453ba956bbf7102963313b945
BSMaidSafe <brian.smith@maidsafe.net>
2014-08-29 14:02:43 +0100

    Removed unrequired files with commented code or that add no new symbols.

--- src/maidsafe/vault/pmid_manager/service.h
+++ src/maidsafe/vault/pmid_manager/service.h
@@ -40,7 +40,6 @@
 #include "maidsafe/vault/types.h"
 #include "maidsafe/vault/pmid_manager/action_delete.h"
 #include "maidsafe/vault/pmid_manager/dispatcher.h"
-#include "maidsafe/vault/pmid_manager/handler.h"
 #include "maidsafe/vault/sync.h"
 #include "maidsafe/vault/pmid_manager/pmid_manager.h"
 #include "maidsafe/vault/pmid_manager/metadata.h"
