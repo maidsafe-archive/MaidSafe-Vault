5964f1eb97e1c1622d186f15f0d9048d36b6323a
Qi Ma <qi.ma@maidsafe.net>
2014-09-04 15:03:07 +0100

    file path shall not be used as disk_root

--- src/maidsafe/vault/chunk_store.cc
+++ src/maidsafe/vault/chunk_store.cc
@@ -70,6 +70,9 @@ DiskUsage InitialiseDiskRoot(const fs::path& disk_root) {
       LOG(kError) << "Can't create disk root at " << disk_root << ": " << error_code.message();
       BOOST_THROW_EXCEPTION(MakeError(CommonErrors::uninitialised));
     }
+  } else if (fs::is_regular_file(disk_root)) {
+    LOG(kError) << "Can't use file " << disk_root << " as disk root ";
+    BOOST_THROW_EXCEPTION(MakeError(CommonErrors::invalid_parameter));
   } else {
     std::vector<fs::path> dirs_to_do;
     dirs_to_do.push_back(disk_root);
