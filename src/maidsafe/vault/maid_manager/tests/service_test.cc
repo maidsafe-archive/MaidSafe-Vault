edc516a2cbe85a917b0e54859e700df7b0ea9c19
BSMaidSafe <brian.smith@maidsafe.net>
2014-11-13 17:24:41 +0000

    Reinstate and update tests. Avoid potential overflow in Median calculation.

--- src/maidsafe/vault/maid_manager/tests/service_test.cc
+++ src/maidsafe/vault/maid_manager/tests/service_test.cc
@@ -68,10 +68,12 @@ class MaidManagerServiceTest : public testing::Test {
     AddAccount(public_maid_.name(), value);
   }
 
-  // template <typename ActionType>
-  // void Commit(const MaidManager::Key& key, const ActionType& action) {
-  //   maid_manager_service_.group_db_.Commit(key, action);
-  // }
+  template <typename ActionType>
+  void Commit(const MaidManager::Key& key, const ActionType& action) {
+    auto it(maid_manager_service_.accounts_.find(key));
+    assert(it != std::end(maid_manager_service_.accounts_));
+    action(it->second);
+  }
 
   MaidManager::Value Get(const MaidManager::Key& key) {
     return maid_manager_service_.accounts_.find(key)->second;
@@ -157,213 +159,105 @@ TEST_F(MaidManagerServiceTest, BEH_PutRequestFromMaidNodeToMaidManager) {
                                      routing::GroupId(MaidNodeId())));
 }
 
-// TEST_F(MaidManagerServiceTest, BEH_PutResponseFromDataManagerToMaidManager) {
-//  auto content(CreateContent<PutResponseFromDataManagerToMaidManager::Contents>());
-//  NodeId data_name_id(content.name.raw_name.string());
-//  auto put_response(CreateMessage<PutResponseFromDataManagerToMaidManager>(content));
-//  auto group_source(CreateGroupSource(data_name_id));
-//  EXPECT_NO_THROW(GroupSendToGroup(&maid_manager_service_, put_response, group_source,
-//                                   routing::GroupId(MaidNodeId())));
-// }
-//
-// TEST_F(MaidManagerServiceTest, BEH_PutFailureFromDataManagerToMaidManager) {
-//  auto content(CreateContent<PutFailureFromDataManagerToMaidManager::Contents>());
-//  NodeId data_name_id(content.name.raw_name.string()), maid_node_id(NodeId::IdType::kRandomId);
-//  auto put_failure(CreateMessage<PutFailureFromDataManagerToMaidManager>(content));
-//  auto group_source(CreateGroupSource(data_name_id));
-//  EXPECT_NO_THROW(GroupSendToGroup(&maid_manager_service_, put_failure, group_source,
-//                                   routing::GroupId(maid_node_id)));
-// }
-//
-// TEST_F(MaidManagerServiceTest, BEH_nfsDeleteRequestFromMaidNodeToMaidManager) {
-//  CreateAccount();
-//  auto content(CreateContent<nfs::DeleteRequestFromMaidNodeToMaidManager::Contents>());
-//  auto delete_request(CreateMessage<nfs::DeleteRequestFromMaidNodeToMaidManager>(content));
-//  EXPECT_NO_THROW(SingleSendsToGroup(&maid_manager_service_, delete_request,
-//                                     routing::SingleSource(MaidNodeId()),
-//                                     routing::GroupId(MaidNodeId())));
-// }
-//
-// TEST_F(MaidManagerServiceTest, BEH_nfsDeleteBranchUntilForkRequestFromMaidNodeToMaidManager) {
-//  auto content(
-//      CreateContent<nfs::DeleteBranchUntilForkRequestFromMaidNodeToMaidManager::Contents>());
-//  auto delete_branch(
-//      CreateMessage<nfs::DeleteBranchUntilForkRequestFromMaidNodeToMaidManager>(content));
-//  EXPECT_NO_THROW(SingleSendsToGroup(&maid_manager_service_, delete_branch,
-//                                     routing::SingleSource(MaidNodeId()),
-//                                     routing::GroupId(MaidNodeId())));
-// }
-//
-// TEST_F(MaidManagerServiceTest, BEH_nfsCreateAccountRequestFromMaidNodeToMaidManager) {
-//  nfs::CreateAccountRequestFromMaidNodeToMaidManager::Contents content(
-//      (passport::PublicMaid(maid_)), (passport::PublicAnmaid(anmaid_)));
-//  auto create_account(CreateMessage<nfs::CreateAccountRequestFromMaidNodeToMaidManager>(content));
-//  EXPECT_NO_THROW(SingleSendsToGroup(&maid_manager_service_, create_account,
-//                                     routing::SingleSource(MaidNodeId()),
-//                                     routing::GroupId(MaidNodeId())));
-// }
-//
-// TEST_F(MaidManagerServiceTest, BEH_nfsRemoveAccountRequestFromMaidNodeToMaidManager) {
-//  nfs::RemoveAccountRequestFromMaidNodeToMaidManager::Contents content(anmaid_);
-//  auto remove_account(CreateMessage<nfs::RemoveAccountRequestFromMaidNodeToMaidManager>(content));
-//  EXPECT_NO_THROW(SingleSendsToGroup(&maid_manager_service_, remove_account,
-//                                     routing::SingleSource(MaidNodeId()),
-//                                     routing::GroupId(MaidNodeId())));
-// }
-//
-// TEST_F(MaidManagerServiceTest, BEH_PmidHealthResponseFromPmidManagerToMaidManager) {
-//  PmidName pmid_name(pmid_.name());
-//  PmidManagerMetadata pmid_manager_metadata(pmid_name);
-//  pmid_manager_metadata.SetAvailableSize(
-//      kTestChunkSize * (RandomUint32() % kAverageChunksStored));
-//  PmidHealthResponseFromPmidManagerToMaidManager::Contents content(
-//      nfs_vault::PmidHealth(pmid_manager_metadata.Serialise()),
-//      nfs_client::ReturnCode(CommonErrors::success));
-//  auto pmid_health_response(
-//      CreateMessage<PmidHealthResponseFromPmidManagerToMaidManager>(content));
-//  auto group_source(CreateGroupSource(NodeId(pmid_name->string())));
-//  EXPECT_NO_THROW(GroupSendToGroup(&maid_manager_service_, pmid_health_response, group_source,
-//                                   routing::GroupId(MaidNodeId())));
-// }
-//
-// TEST_F(MaidManagerServiceTest, BEH_CreateAccount) {
-//  nfs::MessageId message_id(RandomInt32());
-//  ActionCreateAccount action_create_account(message_id);
-//  MaidManager::MetadataKey metadata_key(public_maid_.name());
-//  auto group_source(CreateGroupSource(MaidNodeId()));
-//  auto group_unresolved_action(CreateGroupUnresolvedAction<MaidManager::UnresolvedCreateAccount>(
-//      metadata_key, action_create_account, group_source));
-//  SendSync<MaidManager::UnresolvedCreateAccount>(group_unresolved_action, group_source);
-//  EXPECT_NO_THROW(GetMetadata(public_maid_.name()));
-// }
-//
-// TEST_F(MaidManagerServiceTest, BEH_RemoveAccount) {
-//  CreateAccount();
-//  ActionRemoveAccount action_remove_account((nfs::MessageId(RandomUint32())));
-//  MaidManager::MetadataKey metadata_key(public_maid_.name());
-//  auto group_source(CreateGroupSource(MaidNodeId()));
-//  auto group_unresolved_action(CreateGroupUnresolvedAction<MaidManager::UnresolvedRemoveAccount>(
-//      metadata_key, action_remove_account, group_source));
-//  SendSync<MaidManager::UnresolvedRemoveAccount>(group_unresolved_action, group_source);
-//  EXPECT_ANY_THROW(GetMetadata(public_maid_.name()));
-// }
-//
-// TEST_F(MaidManagerServiceTest, BEH_Put) {
-//  CreateAccount();
-//  ActionMaidManagerPut action_put(kTestChunkSize);
-//  MaidManager::Key key
-//      public_maid_.name(), Identity(RandomString(64)), ImmutableData::Tag::kValue);
-//  auto group_source(CreateGroupSource(MaidNodeId()));
-//  auto group_unresolved_action(
-//      CreateGroupUnresolvedAction<MaidManager::UnresolvedPut>(key, action_put, group_source));
-//  SendSync<MaidManager::UnresolvedPut>(group_unresolved_action, group_source);
-// }
-//
-// TEST_F(MaidManagerServiceTest, BEH_Delete) {
-//  CreateAccount();
-//  ActionMaidManagerDelete action_delete((nfs::MessageId(RandomInt32())));
-//  Identity data_name_id(RandomString(64));
-//  MaidManager::Key key(public_maid_.name(), data_name_id, ImmutableData::Tag::kValue);
-//  Commit(key, ActionMaidManagerPut(kTestChunkSize));
-//  EXPECT_NO_THROW(Get(key));
-//  auto group_source(CreateGroupSource(MaidNodeId()));
-//  auto group_unresolved_action(CreateGroupUnresolvedAction<MaidManager::UnresolvedDelete>(
-//      key, action_delete, group_source));
-//  SendSync<MaidManager::UnresolvedDelete>(group_unresolved_action, group_source);
-// }
-//
-// TEST_F(MaidManagerServiceTest, BEH_UpdatePmid) {
-//  PmidManagerMetadata pmid_manager_metadata(PmidName(Identity(pmid_.name()->string())));
-//  pmid_manager_metadata.stored_count = 1;
-//  pmid_manager_metadata.stored_total_size = kTestChunkSize;
-//  pmid_manager_metadata.lost_count = 2;
-//  pmid_manager_metadata.lost_total_size = 2 * kTestChunkSize;
-//  pmid_manager_metadata.claimed_available_size = 2 << 20;
-//  PmidTotals pmid_totals(std::string(), pmid_manager_metadata);
-//  MaidManager::Metadata metadata(100, std::vector<PmidTotals>(1, pmid_totals));
-//  AddMetadata(public_maid_.name(), metadata);
-//  pmid_manager_metadata.stored_count = 4;
-//  pmid_manager_metadata.stored_total_size = kTestChunkSize * 4;
-//  pmid_manager_metadata.lost_count = 6;
-//  pmid_manager_metadata.lost_total_size = 6 * kTestChunkSize;
-//  pmid_manager_metadata.claimed_available_size = 2 << 10;
-//  PmidTotals updated_pmid_totals(std::string(), pmid_manager_metadata);
-//  MaidManager::Metadata updated_metadata(100, std::vector<PmidTotals>(1, updated_pmid_totals));
-//  ActionMaidManagerUpdatePmidHealth action_update_pmid_health(pmid_manager_metadata);
-//  MaidManager::MetadataKey metadata_key(public_maid_.name());
-//  auto group_source(CreateGroupSource(MaidNodeId()));
-//  auto group_unresolved_action(
-//      CreateGroupUnresolvedAction<MaidManager::UnresolvedUpdatePmidHealth>(
-//        metadata_key, action_update_pmid_health, group_source));
-//  SendSync<MaidManager::UnresolvedUpdatePmidHealth>(group_unresolved_action, group_source);
-//  MaidManager::Metadata stored_metadata(GetMetadata(public_maid_.name()));
-//  EXPECT_TRUE(Equal(stored_metadata, updated_metadata));
-// }
-//
-// TEST_F(MaidManagerServiceTest, BEH_IncrementReferenceCounts) {
-//  const size_t kChunksCount(10);
-//  std::vector<ImmutableData::Name> chunks_list;
-//  std::vector<MaidManager::Key> keys_list;
-//  CreateAccount();
-//  MaidManager::MetadataKey metadata_key(public_maid_.name());
-//  for (size_t index(0); index < kChunksCount; ++index) {
-//    Identity data_name_id(RandomString(64));
-//    MaidManager::Key key(public_maid_.name(), data_name_id, ImmutableData::Tag::kValue);
-//    Commit(key, ActionMaidManagerPut(kTestChunkSize));
-//    EXPECT_NO_THROW(Get(key));
-//    chunks_list.push_back(ImmutableData::Name(data_name_id));
-//    keys_list.push_back(key);
-//  }
-//  ActionMaidManagerIncrementReferenceCounts action_increment_reference(
-//      (nfs_vault::DataNames(chunks_list)));
-//  auto group_source(CreateGroupSource(MaidNodeId()));
-//  auto group_unresolved_action(
-//      CreateGroupUnresolvedAction<MaidManager::UnresolvedIncrementReferenceCounts>(
-//          metadata_key, action_increment_reference, group_source));
-//  SendSync<MaidManager::UnresolvedIncrementReferenceCounts>(
-//      group_unresolved_action, group_source);
-//  for (size_t index(0); index < kChunksCount; ++index) {
-//    auto value(Get(keys_list[index]));
-//    EXPECT_TRUE(value.count() == 2);
-//  }
-// }
-//
-// TEST_F(MaidManagerServiceTest, BEH_DecrementReferenceCounts) {
-//  const size_t kChunksCount(10);
-//  std::vector<ImmutableData::Name> chunks_list;
-//  std::vector<MaidManager::Key> keys_list;
-//  CreateAccount();
-//  MaidManager::MetadataKey metadata_key(public_maid_.name());
-//  for (size_t index(0); index < kChunksCount; ++index) {
-//    Identity data_name_id(RandomString(64));
-//    MaidManager::Key key(public_maid_.name(), data_name_id, ImmutableData::Tag::kValue);
-//    Commit(key, ActionMaidManagerPut(kTestChunkSize));
-//    EXPECT_NO_THROW(Get(key));
-//    chunks_list.push_back(ImmutableData::Name(data_name_id));
-//    keys_list.push_back(key);
-//  }
-//
-//  for (size_t index(0); index < kChunksCount; ++index)
-//    Commit(keys_list[index], ActionMaidManagerIncrementReferenceCount());
-//
-//  for (size_t index(0); index < kChunksCount; ++index) {
-//    auto value(Get(keys_list[index]));
-//    EXPECT_TRUE(value.count() == 2);
-//  }
-//
-//  ActionMaidManagerDecrementReferenceCounts action_decrement_reference(
-//      (nfs_vault::DataNames(chunks_list)));
-//  auto group_source(CreateGroupSource(MaidNodeId()));
-//  auto group_unresolved_action(
-//      CreateGroupUnresolvedAction<MaidManager::UnresolvedDecrementReferenceCounts>(
-//          metadata_key, action_decrement_reference, group_source));
-//  SendSync<MaidManager::UnresolvedDecrementReferenceCounts>
-//      group_unresolved_action, group_source);
-//  for (size_t index(0); index < kChunksCount; ++index) {
-//    auto value(Get(keys_list[index]));
-//    EXPECT_TRUE(value.count() == 1);
-//  }
-// }
+TEST_F(MaidManagerServiceTest, BEH_PutResponseFromDataManagerToMaidManager) {
+  auto content(CreateContent<PutResponseFromDataManagerToMaidManager::Contents>());
+  NodeId data_name_id(content.name.raw_name.string());
+  auto put_response(CreateMessage<PutResponseFromDataManagerToMaidManager>(content));
+  auto group_source(CreateGroupSource(data_name_id));
+  EXPECT_NO_THROW(GroupSendToGroup(&maid_manager_service_, put_response, group_source,
+                                   routing::GroupId(MaidNodeId())));
+}
+
+TEST_F(MaidManagerServiceTest, BEH_PutFailureFromDataManagerToMaidManager) {
+  auto content(CreateContent<PutFailureFromDataManagerToMaidManager::Contents>());
+  NodeId data_name_id(content.name.raw_name.string()), maid_node_id(NodeId::IdType::kRandomId);
+  auto put_failure(CreateMessage<PutFailureFromDataManagerToMaidManager>(content));
+  auto group_source(CreateGroupSource(data_name_id));
+  EXPECT_NO_THROW(GroupSendToGroup(&maid_manager_service_, put_failure, group_source,
+                                   routing::GroupId(maid_node_id)));
+}
+
+TEST_F(MaidManagerServiceTest, BEH_nfsDeleteRequestFromMaidNodeToMaidManager) {
+  CreateAccount();
+  auto content(CreateContent<nfs::DeleteRequestFromMaidNodeToMaidManager::Contents>());
+  auto delete_request(CreateMessage<nfs::DeleteRequestFromMaidNodeToMaidManager>(content));
+  EXPECT_NO_THROW(SingleSendsToGroup(&maid_manager_service_, delete_request,
+                                     routing::SingleSource(MaidNodeId()),
+                                     routing::GroupId(MaidNodeId())));
+}
+
+TEST_F(MaidManagerServiceTest, BEH_nfsDeleteBranchUntilForkRequestFromMaidNodeToMaidManager) {
+  auto content(
+      CreateContent<nfs::DeleteBranchUntilForkRequestFromMaidNodeToMaidManager::Contents>());
+  auto delete_branch(
+      CreateMessage<nfs::DeleteBranchUntilForkRequestFromMaidNodeToMaidManager>(content));
+  EXPECT_NO_THROW(SingleSendsToGroup(&maid_manager_service_, delete_branch,
+                                     routing::SingleSource(MaidNodeId()),
+                                     routing::GroupId(MaidNodeId())));
+}
+
+TEST_F(MaidManagerServiceTest, BEH_nfsCreateAccountRequestFromMaidNodeToMaidManager) {
+  nfs::CreateAccountRequestFromMaidNodeToMaidManager::Contents content(
+      (passport::PublicMaid(maid_)), (passport::PublicAnmaid(anmaid_)));
+  auto create_account(CreateMessage<nfs::CreateAccountRequestFromMaidNodeToMaidManager>(content));
+  EXPECT_NO_THROW(SingleSendsToGroup(&maid_manager_service_, create_account,
+                                     routing::SingleSource(MaidNodeId()),
+                                     routing::GroupId(MaidNodeId())));
+}
+
+TEST_F(MaidManagerServiceTest, BEH_nfsRemoveAccountRequestFromMaidNodeToMaidManager) {
+  nfs::RemoveAccountRequestFromMaidNodeToMaidManager::Contents content(anmaid_);
+  auto remove_account(CreateMessage<nfs::RemoveAccountRequestFromMaidNodeToMaidManager>(content));
+  EXPECT_NO_THROW(SingleSendsToGroup(&maid_manager_service_, remove_account,
+                                     routing::SingleSource(MaidNodeId()),
+                                     routing::GroupId(MaidNodeId())));
+}
+
+TEST_F(MaidManagerServiceTest, BEH_CreateAccount) {
+  nfs::MessageId message_id(RandomInt32());
+  ActionCreateAccount action_create_account(message_id);
+  MaidManager::MetadataKey metadata_key(public_maid_.name());
+  auto group_source(CreateGroupSource(MaidNodeId()));
+  auto group_unresolved_action(CreateGroupUnresolvedAction<MaidManager::UnresolvedCreateAccount>(
+      metadata_key, action_create_account, group_source));
+  SendSync<MaidManager::UnresolvedCreateAccount>(group_unresolved_action, group_source);
+  EXPECT_NO_THROW(GetValue(public_maid_.name()));
+}
+
+TEST_F(MaidManagerServiceTest, BEH_RemoveAccount) {
+  CreateAccount();
+  ActionRemoveAccount action_remove_account((nfs::MessageId(RandomUint32())));
+  MaidManager::MetadataKey metadata_key(public_maid_.name());
+  auto group_source(CreateGroupSource(MaidNodeId()));
+  auto group_unresolved_action(CreateGroupUnresolvedAction<MaidManager::UnresolvedRemoveAccount>(
+      metadata_key, action_remove_account, group_source));
+  SendSync<MaidManager::UnresolvedRemoveAccount>(group_unresolved_action, group_source);
+  EXPECT_ANY_THROW(GetValue(public_maid_.name()));
+}
+
+TEST_F(MaidManagerServiceTest, BEH_Put) {
+  CreateAccount();
+  ActionMaidManagerPut action_put(kTestChunkSize);
+  MaidManager::SyncKey key(
+    public_maid_.name(), Identity(RandomString(64)), ImmutableData::Tag::kValue);
+  auto group_source(CreateGroupSource(MaidNodeId()));
+  auto group_unresolved_action(
+      CreateGroupUnresolvedAction<MaidManager::UnresolvedPut>(key, action_put, group_source));
+  SendSync<MaidManager::UnresolvedPut>(group_unresolved_action, group_source);
+}
+
+TEST_F(MaidManagerServiceTest, BEH_Delete) {
+  CreateAccount();
+  ActionMaidManagerDelete action_delete((nfs::MessageId(RandomInt32())));
+  Identity data_name_id(RandomString(64));
+  MaidManager::SyncKey key(public_maid_.name(), data_name_id, ImmutableData::Tag::kValue);
+  Commit(key.group_name(), ActionMaidManagerPut(kTestChunkSize));
+  EXPECT_NO_THROW(Get(key.group_name()));
+  auto group_source(CreateGroupSource(MaidNodeId()));
+  auto group_unresolved_action(CreateGroupUnresolvedAction<MaidManager::UnresolvedDelete>(
+      key, action_delete, group_source));
+  SendSync<MaidManager::UnresolvedDelete>(group_unresolved_action, group_source);
+}
 
 }  //  namespace test
 
