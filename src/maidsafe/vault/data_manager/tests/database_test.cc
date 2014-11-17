6e3a07eca5b2310820d95e3d6564c42de4ca1b96
maqi <qi.ma@maidsafe.net>
2014-11-17 09:43:24 +0000

    update DataManager service test

--- src/maidsafe/vault/data_manager/tests/database_test.cc
+++ src/maidsafe/vault/data_manager/tests/database_test.cc
@@ -1,1 +1,110 @@
+/*  Copyright 2014 MaidSafe.net limited
+
+    This MaidSafe Software is licensed to you under (1) the MaidSafe.net Commercial License,
+    version 1.0 or later, or (2) The General Public License (GPL), version 3, depending on which
+    licence you accepted on initial access to the Software (the "Licences").
+
+    By contributing code to the MaidSafe Software, or to this project generally, you agree to be
+    bound by the terms of the MaidSafe Contributor Agreement, version 1.0, found in the root
+    directory of this project at LICENSE, COPYING and CONTRIBUTOR respectively and also
+    available at: http://www.maidsafe.net/licenses
+
+    Unless required by applicable law or agreed to in writing, the MaidSafe Software distributed
+    under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+    OF ANY KIND, either express or implied.
+
+    See the Licences for the specific language governing permissions and limitations relating to
+    use of the MaidSafe Software.                                                                 */
+
+#include "maidsafe/common/test.h"
+#include "maidsafe/passport/passport.h"
+
+#include "maidsafe/vault/data_manager/datamanager_database.h"
+#include "maidsafe/vault/data_manager/data_manager.h"
+#include "maidsafe/vault/tests/tests_utils.h"
+#include "maidsafe/vault/utils.h"
+
+namespace maidsafe {
+
+namespace vault {
+
+namespace test {
+
+class DataManagerDatabaseTest : public testing::Test {
+ public:
+  DataManagerDatabaseTest()
+      : kTestRoot_(maidsafe::test::CreateTestPath("MaidSafe_Test_Vault")) {}
+
+ protected:
+  const maidsafe::test::TestPath kTestRoot_;
+};
+
+TEST_F(DataManagerDatabaseTest, BEH_GetFromEmpty) {
+  DataManagerDataBase db(UniqueDbPath(*kTestRoot_));
+  ImmutableData data(NonEmptyString(RandomString(kTestChunkSize)));
+  DataManager::Key key(data.name());
+  EXPECT_ANY_THROW(db.Get(key));
+}
+
+TEST_F(DataManagerDatabaseTest, BEH_AddPmid) {
+  DataManagerDataBase db(UniqueDbPath(*kTestRoot_));
+  std::vector<PmidName> pmid_nodes;
+  for (int i(0); i < 10; ++i) {
+    pmid_nodes.push_back(PmidName(Identity(RandomString(64))));
+    LOG(kVerbose) << "Generated : " << HexSubstr(pmid_nodes.back()->string());
+  }
+
+  ImmutableData data(NonEmptyString(RandomString(kTestChunkSize)));
+  DataManager::Key key(data.name());
+
+  for (auto& pmid_node : pmid_nodes) {
+    ActionDataManagerAddPmid action_add_pmid(pmid_node, kTestChunkSize);
+    db.Commit(key, action_add_pmid);
+  }
+  auto results(db.Get(key).AllPmids());
+  EXPECT_EQ(pmid_nodes.size(), results.size());
+  // The order of insertion shall get retained
+  for (size_t i(0); i < results.size(); ++i) {
+    LOG(kVerbose) << "currently checking : " << HexSubstr(results[i]->string());
+    EXPECT_EQ(pmid_nodes[i], results[i]);
+  }
+}
+
+//TEST_F(DataManagerDatabaseTest, BEH_RemovePmid) {
+//  DataManagerDataBase db(UniqueDbPath(*kTestRoot_));
+//  // Action Add Pmid
+//  ImmutableData data(NonEmptyString(RandomString(kTestChunkSize)));
+//  DataManager::Key key(data.name());
+//  EXPECT_ANY_THROW(db.Get(key));
+
+//  // Action Delete
+
+//  // Action Remove Pmid
+//}
+
+//TEST_F(DataManagerDatabaseTest, BEH_Delete) {
+//  DataManagerDataBase db(UniqueDbPath(*kTestRoot_));
+//  // Action Add Pmid
+//  ImmutableData data(NonEmptyString(RandomString(kTestChunkSize)));
+//  DataManager::Key key(data.name());
+//  EXPECT_ANY_THROW(db.Get(key));
+
+//  // Action Delete
+
+//  // Action Remove Pmid
+//}
+
+
+TEST_F(DataManagerDatabaseTest, BEH_GetRelatedAccountsFromEmpty) {
+  DataManagerDataBase db(UniqueDbPath(*kTestRoot_));
+  PmidName pmid_name(Identity(RandomString(64)));
+  auto result(db.GetRelatedAccounts(pmid_name));
+  EXPECT_TRUE(result.empty());
+}
+
+}  //  namespace test
+
+}  //  namespace vault
+
+}  //  namespace maidsafe
 
