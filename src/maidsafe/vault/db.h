58a7e96ab2ab321b0ee0e15cc7500ca1690fee06
moadeli <mmoadeli@maidsafe.net>
2014-12-08 17:28:50 +0000

    disabling exception throw when a node is not updated yet.

--- src/maidsafe/vault/db.h
+++ src/maidsafe/vault/db.h
@@ -205,8 +205,9 @@ template <typename Key, typename Value>
 Value Db<Key, Value>::Get(const Key& key) {
   std::string value_string;
   sqlitedb_->Get(key.ToFixedWidthString().string(), value_string);
-  if (value_string.empty())
+  if (value_string.empty()) {
     BOOST_THROW_EXCEPTION(MakeError(VaultErrors::no_such_account));
+  }
   return Value(value_string);
 }
 
