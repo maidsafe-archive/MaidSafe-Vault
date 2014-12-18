5e031d3d3df6f9b3f5a8ef5fd26ef0989d069815
BSMaidSafe <brian.smith@maidsafe.net>
2014-12-18 15:36:56 +0000

    Updated in line with comments.

--- src/maidsafe/vault/parameters.h
+++ src/maidsafe/vault/parameters.h
@@ -42,8 +42,6 @@ struct Parameters {
   static int integrity_check_string_size;
   // Default network timeout
   static const std::chrono::milliseconds kDefaultTimeout;
-  // Default accumulator request lifetime
-  static std::chrono::seconds default_lifetime;
   // Prune account transfer when the size is divisible by below factor
   static unsigned int account_transfer_cleanup_factor;
   // Removes entries which have been longer than below factor
