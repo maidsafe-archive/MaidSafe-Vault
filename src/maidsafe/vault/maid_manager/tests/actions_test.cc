3eda67dde6b7b1dd7500cecfeb8b0102a9417881
BSMaidSafe <brian.smith@maidsafe.net>
2015-01-06 16:20:06 +0000

    Added account actions to tests.

--- src/maidsafe/vault/maid_manager/tests/actions_test.cc
+++ src/maidsafe/vault/maid_manager/tests/actions_test.cc
@@ -23,6 +23,7 @@
 #include "maidsafe/nfs/types.h"
 #include "maidsafe/vault/maid_manager/action_put.h"
 #include "maidsafe/vault/maid_manager/action_delete.h"
+#include "maidsafe/vault/maid_manager/action_create_remove_account.h"
 #include "maidsafe/vault/maid_manager/maid_manager.h"
 #include "maidsafe/vault/maid_manager/value.h"
 
@@ -34,75 +35,170 @@ namespace test {
 
 
 TEST(MaidManagerActionsTest, BEH_Construct) {
-  decltype(ActionMaidManagerPut::kSize) size(1);
-  ActionMaidManagerPut action_put(size);
-  ASSERT_EQ(nfs::MessageAction::kPutRequest, action_put.kActionId);
-  ASSERT_EQ(size, action_put.kSize);
+  {
+    // put
+    decltype(ActionMaidManagerPut::kSize) size(1);
+    ActionMaidManagerPut action(size);
+    ASSERT_EQ(nfs::MessageAction::kPutRequest, action.kActionId);
+    ASSERT_EQ(size, action.kSize);
 
-  ASSERT_NO_THROW(ActionMaidManagerPut action_put0(action_put));
-//  ASSERT_NO_THROW(ActionMaidManagerPut(action_put));
+    ASSERT_NO_THROW(ActionMaidManagerPut action_(action));
+    ASSERT_NO_THROW(ActionMaidManagerPut(std::move(action)));
+  }
+  {
+    // delete
+    decltype(ActionMaidManagerDelete::kMessageId) message_id(RandomInt32());
+    ActionMaidManagerDelete action(message_id);
+    ASSERT_EQ(nfs::MessageAction::kDeleteRequest, action.kActionId);
+    ASSERT_EQ(message_id, action.kMessageId);
 
-  decltype(ActionMaidManagerDelete::kMessageId) message_id(RandomInt32());
-  ActionMaidManagerDelete action_delete(message_id);
-  ASSERT_EQ(nfs::MessageAction::kDeleteRequest, action_delete.kActionId);
-  ASSERT_EQ(message_id, action_delete.kMessageId);
+    ASSERT_NO_THROW(ActionMaidManagerDelete action_(action));
+    ASSERT_NO_THROW(ActionMaidManagerDelete(std::move(action)));
+  }
+  {
+    // create/remove account true
+    decltype(ActionCreateRemoveAccount<true>::kMessageId) message_id(RandomInt32());
+    ActionCreateRemoveAccount<true> action(message_id);
+    ASSERT_EQ(nfs::MessageAction::kRemoveAccountRequest, action.kActionId);
+    ASSERT_EQ(message_id, action.kMessageId);
 
-  ASSERT_NO_THROW(ActionMaidManagerDelete action_delete0(action_delete));
-//  ASSERT_NO_THROW(ActionMaidManagerDelete(action_delete));
+    ASSERT_NO_THROW(ActionCreateRemoveAccount<true> action_(action));
+    ASSERT_NO_THROW(ActionCreateRemoveAccount<true>(std::move(action)));
+  }
+  {
+    // create/remove account false
+    decltype(ActionCreateRemoveAccount<false>::kMessageId) message_id(RandomInt32());
+    ActionCreateRemoveAccount<false> action(message_id);
+    ASSERT_EQ(nfs::MessageAction::kCreateAccountRequest, action.kActionId);
+    ASSERT_EQ(message_id, action.kMessageId);
+
+    ASSERT_NO_THROW(ActionCreateRemoveAccount<false> action_(action));
+    ASSERT_NO_THROW(ActionCreateRemoveAccount<false>(std::move(action)));
+  }
 }
 
 TEST(MaidManagerActionsTest, BEH_EqualityInequality) {
-  decltype(ActionMaidManagerPut::kSize) size1(1), size2(2);
-  ActionMaidManagerPut action1_put(size1), action2_put(size2), action3_put(size1);
+  {
+    // put
+    decltype(ActionMaidManagerPut::kSize) size1(1), size2(2);
+    ActionMaidManagerPut action1(size1), action2(size2), action3(size1);
   
-  ASSERT_EQ(nfs::MessageAction::kPutRequest, action1_put.kActionId);
-  ASSERT_EQ(size1, action1_put.kSize);
-  ASSERT_EQ(nfs::MessageAction::kPutRequest, action2_put.kActionId);
-  ASSERT_EQ(size2, action2_put.kSize);
-  ASSERT_EQ(nfs::MessageAction::kPutRequest, action3_put.kActionId);
-  ASSERT_EQ(size1, action3_put.kSize);
+    ASSERT_EQ(nfs::MessageAction::kPutRequest, action1.kActionId);
+    ASSERT_EQ(size1, action1.kSize);
+    ASSERT_EQ(nfs::MessageAction::kPutRequest, action2.kActionId);
+    ASSERT_EQ(size2, action2.kSize);
+    ASSERT_EQ(nfs::MessageAction::kPutRequest, action3.kActionId);
+    ASSERT_EQ(size1, action3.kSize);
 
-  ASSERT_FALSE(action1_put == action2_put);
-  ASSERT_TRUE(action1_put == action3_put);
-  ASSERT_TRUE(action1_put != action2_put);
-  ASSERT_FALSE(action1_put != action3_put);
-  
-  decltype(ActionMaidManagerDelete::kMessageId) message1_id(RandomInt32()),
-                                                message2_id(RandomInt32());
-  ActionMaidManagerDelete action1_delete(message1_id), action2_delete(message2_id),
-                          action3_delete(message1_id);
+    ASSERT_FALSE(action1 == action2);
+    ASSERT_TRUE(action1 == action3);
+    ASSERT_TRUE(action1 != action2);
+    ASSERT_FALSE(action1 != action3);
+  }
+  {
+    // delete
+    decltype(ActionMaidManagerDelete::kMessageId) message1_id(RandomInt32()),
+                                                  message2_id(RandomInt32());
+    ActionMaidManagerDelete action1(message1_id), action2(message2_id),
+                            action3(message1_id);
 
-  ASSERT_EQ(nfs::MessageAction::kDeleteRequest, action1_delete.kActionId);
-  ASSERT_EQ(message1_id, action1_delete.kMessageId);
-  ASSERT_EQ(nfs::MessageAction::kDeleteRequest, action2_delete.kActionId);
-  ASSERT_EQ(message2_id, action2_delete.kMessageId);
-  ASSERT_EQ(nfs::MessageAction::kDeleteRequest, action3_delete.kActionId);
-  ASSERT_EQ(message1_id, action3_delete.kMessageId);
+    ASSERT_EQ(nfs::MessageAction::kDeleteRequest, action1.kActionId);
+    ASSERT_EQ(message1_id, action1.kMessageId);
+    ASSERT_EQ(nfs::MessageAction::kDeleteRequest, action2.kActionId);
+    ASSERT_EQ(message2_id, action2.kMessageId);
+    ASSERT_EQ(nfs::MessageAction::kDeleteRequest, action3.kActionId);
+    ASSERT_EQ(message1_id, action3.kMessageId);
 
-  ASSERT_FALSE(action1_delete == action2_delete);
-  ASSERT_TRUE(action1_delete == action3_delete);
-  ASSERT_TRUE(action1_delete != action2_delete);
-  ASSERT_FALSE(action1_delete != action3_delete);
+    ASSERT_FALSE(action1 == action2);
+    ASSERT_TRUE(action1 == action3);
+    ASSERT_TRUE(action1 != action2);
+    ASSERT_FALSE(action1 != action3);
+  }
+  {
+    // create/remove account true
+    decltype(ActionCreateRemoveAccount<true>::kMessageId) message1_id(RandomInt32()),
+                                                          message2_id(RandomInt32());
+    ActionCreateRemoveAccount<true> action1(message1_id), action2(message2_id),
+                                    action3(message1_id);
+
+    ASSERT_EQ(nfs::MessageAction::kRemoveAccountRequest, action1.kActionId);
+    ASSERT_EQ(message1_id, action1.kMessageId);
+    ASSERT_EQ(nfs::MessageAction::kRemoveAccountRequest, action2.kActionId);
+    ASSERT_EQ(message2_id, action2.kMessageId);
+    ASSERT_EQ(nfs::MessageAction::kRemoveAccountRequest, action3.kActionId);
+    ASSERT_EQ(message1_id, action3.kMessageId);
+
+    ASSERT_FALSE(action1 == action2);
+    ASSERT_TRUE(action1 == action3);
+    ASSERT_TRUE(action1 != action2);
+    ASSERT_FALSE(action1 != action3);
+  }
+  {
+    // create/remove account false
+    decltype(ActionCreateRemoveAccount<false>::kMessageId) message1_id(RandomInt32()),
+                                                           message2_id(RandomInt32());
+    ActionCreateRemoveAccount<false> action1(message1_id), action2(message2_id),
+                                     action3(message1_id);
+
+    ASSERT_EQ(nfs::MessageAction::kCreateAccountRequest, action1.kActionId);
+    ASSERT_EQ(message1_id, action1.kMessageId);
+    ASSERT_EQ(nfs::MessageAction::kCreateAccountRequest, action2.kActionId);
+    ASSERT_EQ(message2_id, action2.kMessageId);
+    ASSERT_EQ(nfs::MessageAction::kCreateAccountRequest, action3.kActionId);
+    ASSERT_EQ(message1_id, action3.kMessageId);
+
+    ASSERT_FALSE(action1 == action2);
+    ASSERT_TRUE(action1 == action3);
+    ASSERT_TRUE(action1 != action2);
+    ASSERT_FALSE(action1 != action3);
+  }
 }
 
 TEST(MaidManagerActionsTest, BEH_SerialiseParse) {
-  decltype(ActionMaidManagerPut::kSize) size(1);
-  ActionMaidManagerPut action_put(size); 
-  std::string serialised_action_put;
-  ASSERT_THROW(ActionMaidManagerPut action0_put(serialised_action_put), maidsafe_error);
-  ASSERT_NO_THROW(serialised_action_put = action_put.Serialise());
-  ActionMaidManagerPut action0_put(serialised_action_put);
-  ASSERT_EQ(nfs::MessageAction::kPutRequest, action0_put.kActionId);
-  ASSERT_EQ(size, action0_put.kSize);
-
-  decltype(ActionMaidManagerDelete::kMessageId) message_id(RandomInt32());
-  ActionMaidManagerDelete action_delete(message_id);
-  std::string serialised_action_delete;
-  ASSERT_THROW(ActionMaidManagerDelete action0_delete(serialised_action_delete), maidsafe_error);
-  ASSERT_NO_THROW(serialised_action_delete = action_delete.Serialise());
-  ActionMaidManagerDelete action0_delete(serialised_action_delete);
-  ASSERT_EQ(nfs::MessageAction::kDeleteRequest, action0_delete.kActionId);
-  ASSERT_EQ(message_id, action0_delete.kMessageId);
+  {
+    // put
+    decltype(ActionMaidManagerPut::kSize) size(1);
+    ActionMaidManagerPut action(size);
+    std::string serialised_action;
+    ASSERT_THROW(ActionMaidManagerPut action0(serialised_action), maidsafe_error);
+    ASSERT_NO_THROW(serialised_action = action.Serialise());
+    ActionMaidManagerPut action0(serialised_action);
+    ASSERT_EQ(nfs::MessageAction::kPutRequest, action0.kActionId);
+    ASSERT_EQ(size, action0.kSize);
+  }
+  {
+    // delete
+    decltype(ActionMaidManagerDelete::kMessageId) message_id(RandomInt32());
+    ActionMaidManagerDelete action(message_id);
+    std::string serialised_action;
+    ASSERT_THROW(ActionMaidManagerDelete action0(serialised_action), maidsafe_error);
+    ASSERT_NO_THROW(serialised_action = action.Serialise());
+    ActionMaidManagerDelete action0(serialised_action);
+    ASSERT_EQ(nfs::MessageAction::kDeleteRequest, action0.kActionId);
+    ASSERT_EQ(message_id, action0.kMessageId);
+  }
+  {
+    // create/remove account true
+    decltype(ActionCreateRemoveAccount<true>::kMessageId) message_id(RandomInt32());
+    ActionCreateRemoveAccount<true> action(message_id);
+    std::string serialised_action;
+    ASSERT_THROW(ActionCreateRemoveAccount<true> action0(serialised_action), maidsafe_error);
+    ASSERT_NO_THROW(serialised_action = action.Serialise());
+    ActionCreateRemoveAccount<true> action0(serialised_action);
+    ASSERT_EQ(nfs::MessageAction::kRemoveAccountRequest, action0.kActionId);
+    ASSERT_EQ(message_id, action0.kMessageId);
+  }
+  {
+    // create/remove account false
+    decltype(ActionCreateRemoveAccount<false>::kMessageId) message_id(RandomInt32());
+    ActionCreateRemoveAccount<false> action(message_id);
+    std::string serialised_action;
+    ASSERT_THROW(ActionCreateRemoveAccount<false> action0(serialised_action), maidsafe_error);
+    ASSERT_NO_THROW(serialised_action = action.Serialise());
+    ActionCreateRemoveAccount<false> action0(serialised_action);
+    ASSERT_EQ(nfs::MessageAction::kCreateAccountRequest, action0.kActionId);
+    ASSERT_EQ(message_id, action0.kMessageId);
+  }
 }
 
 TEST(MaidManagerActionsTest, BEH_PutOperator) {
